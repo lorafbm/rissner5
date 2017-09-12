3129750491d272881771b051a05e5046b0f722b7
lorafbm <gsl_billdind@mail.ru>
2017-09-12 23:31:53 +0300

    new

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+class Core
+{
+    static $DATE      = 2016;
+    static $DATAPRICE = 01;
+    static $CONT      = 'modules';
+    static $SKIN      = 'default';
+
+    static $DB_NAME   = 'main';
+    static $DB_LOGIN  = 'root';
+    static $DB_PASS   = '';
+    static $DB_LOCAL  = 'localhost';
+    static $DOMAIN    = 'http://localhost';
+   /* static $DB_NAME   = 'lora';
+    static $DB_LOGIN  = 'lora';
+    static $DB_PASS   = 'D3v40nka-da';
+    static $DB_LOCAL  = 'localhost';
+    static $DOMAIN    = 'http://lora.school-php.com';*/
+
+    static $JS    = array();
+    static $CSS   = array();
+    static $META  = array(
+        'title'      => 'rissner52075',
+        'description'=> 'd',
+        'keywords'   => 'k'
+   );
+}
+
